1a62539cff041d45c827d8c368980c04f22fd6fa
gmaanx <giamando09@gmail.com>
2026-01-03 17:45:24 +0700

    implement menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 
-import Navbar from './components/Navbar.jsx';
-import Hero from './components/Hero.jsx';
-import Cocktails from './components/Cocktails.jsx';
-import About from './components/About.jsx';
-import Art from './components/Art.jsx';
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <Hero/>
-      <Cocktails/>
-      <About/>
-      <Art/>
+      <Navbar />
+	    <Hero />
+	    <Cocktails />
+	    <About />
+	    <Art />
+	    <Menu />
     </main>
   )
 }
